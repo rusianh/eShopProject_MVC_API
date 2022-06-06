be90e5eddef07df5bb017f11b5051c89015aabea
Cris Dev <vva1994@gmail.com>
2022-06-06 14:32:27 +0700

    Add project files.

---
+++ eShopSolution.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using eShopSolution.WebApp.Models;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Http;
+using LazZiya.ExpressLocalization;
+using eShopSolution.ApiIntegration;
+using System.Globalization;
+using eShopSolution.Utilities.Constants;
+
+namespace eShopSolution.WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ISharedCultureLocalizer _loc;
+        private readonly ISlideApiClient _slideApiClient;
+        private readonly IProductApiClient _productApiClient;
+
+        public HomeController(ILogger<HomeController> logger,
+            ISlideApiClient slideApiClient,
+            IProductApiClient productApiClient)
+        {
+            _logger = logger;
+            _slideApiClient = slideApiClient;
+            _productApiClient = productApiClient;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var culture = CultureInfo.CurrentCulture.Name;
+            var viewModel = new HomeViewModel
+            {
+                Slides = await _slideApiClient.GetAll(),
+                FeaturedProducts = await _productApiClient.GetFeaturedProducts(culture, SystemConstants.ProductSettings.NumberOfFeaturedProducts),
+                LatestProducts = await _productApiClient.GetLatestProducts(culture, SystemConstants.ProductSettings.NumberOfLatestProducts),
+            };
+
+            return View(viewModel);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult SetCultureCookie(string cltr, string returnUrl)
+        {
+            Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(cltr)),
+                new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) }
+                );
+
+            return LocalRedirect(returnUrl);
+        }
+    }
+}
---
+++ eShopSolution.WebApp/wwwroot/js/site.js
@@ -0,0 +1,43 @@
+﻿var SiteController = function () {
+    this.initialize = function () {
+        regsiterEvents();
+        loadCart();
+    }
+    function loadCart() {
+        const culture = $('#hidCulture').val();
+        $.ajax({
+            type: "GET",
+            url: "/" + culture + '/Cart/GetListItems',
+            success: function (res) {
+                $('#lbl_number_items_header').text(res.length);
+            }
+        });
+    }
+    function regsiterEvents() {
+        // Write your JavaScript code.
+        $('body').on('click', '.btn-add-cart', function (e) {
+            e.preventDefault();
+            const culture = $('#hidCulture').val();
+            const id = $(this).data('id');
+            $.ajax({
+                type: "POST",
+                url: "/" + culture + '/Cart/AddToCart',
+                data: {
+                    id: id,
+                    languageId: culture
+                },
+                success: function (res) {
+                    $('#lbl_number_items_header').text(res.length);
+                },
+                error: function (err) {
+                    console.log(err);
+                }
+            });
+        });
+    }
+}
+
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
